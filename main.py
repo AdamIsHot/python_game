291ec1238f3ad4091dd83261786028c353101ae0
AdamIsHot <adam.jerhot@gmail.com>
2022-10-10 16:41:36 +0200

    had se sam odsebe pohybuje

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ from pygame. locals import *
 import time
 import movement
 
-class Geek:
-    def __init__(self, age = 0):
-         self._age = age
-      
-    # getter method
-    def get_age(self):
-        return self._age
-      
-    # setter method
-    def set_age(self, x):
-        self._age = x
-  
-raj = Geek()
-
-
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption('PySnake')
@@ -26,39 +11,50 @@ DEFAULT_IMAGE_SIZE = (50, 50)
 snake = pygame.transform.scale(image, DEFAULT_IMAGE_SIZE)
 
 
-
 x = 400
 y = 400
 velocity = 50
+direction = 0
+
   
 running = True
 while running:
     screen.fill((255, 255, 255))
     screen.blit(snake, (x, y))
 
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
-
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    raj.set_age(21)
-                    x -= velocity
+                if event.key == pygame.K_a:
+                    direction = 1
   
-                if event.key == pygame.K_RIGHT:
-                    x += velocity
-                    pygame.display.update()
+                if event.key == pygame.K_d:
+                    direction = 2
+
+                if event.key == pygame.K_w:
+                    direction = 3
   
-                if event.key == pygame.K_UP:
-                    y -= velocity
-                    pygame.display.update()
-  
-                if event.key == pygame.K_DOWN:
-                    y += velocity
-                    pygame.display.update()
-        
-        print(raj.get_age())
-        pygame.display.update()
+                if event.key == pygame.K_s:
+                    direction = 4
+    
+
+    if direction == 1:
+        x -= velocity
+    if direction == 2:
+        x += velocity
+    if direction == 3:
+        y -= velocity
+    if direction == 4:
+        y += velocity
+
+    screen.fill((255, 255, 255))
+    screen.blit(snake, (x, y))
+    
+    pygame.display.update()
+    pygame.time.wait(150)
         
     
