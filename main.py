33bc887b48b2e7488e953aef26673c264119bf9e
AdamIsHot <adam.jerhot@gmail.com>
2022-10-13 17:21:46 +0200

    code simplification

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from difflib import SequenceMatcher
 from tkinter import LEFT
 import pygame
 from pygame. locals import *
-import time
-import movement
+import movement as m
 import random
 
 pygame.init()
@@ -15,8 +14,6 @@ DEFAULT_IMAGE_SIZE = (50, 50)
 snake = pygame.transform.scale(image, DEFAULT_IMAGE_SIZE)
 apple = pygame.transform.scale(image2, DEFAULT_IMAGE_SIZE)
 
-x = 400
-y = 400
 velocity = 50
 direction = 0
 
@@ -30,6 +27,8 @@ horizontal_movement = False
 apple_positons = [0,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750]
 apple_x = random.choice(apple_positons)
 apple_y = random.choice(apple_positons)
+x = 400
+y = 400
 
 while running:
     screen.fill((255, 255, 255))
@@ -39,27 +38,16 @@ while running:
 
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a and not horizontal_movement:
-                    direction = 1
-                    horizontal_movement = True
+                    direction = 1; horizontal_movement = True
                 if event.key == pygame.K_d and not horizontal_movement:
-                    direction = 2
-                    horizontal_movement = True
+                    direction = 2; horizontal_movement = True
                 if event.key == pygame.K_w and horizontal_movement:
-                    direction = 3
-                    horizontal_movement = False
+                    direction = 3; horizontal_movement = False
                 if event.key == pygame.K_s and horizontal_movement:
-                    direction = 4
-                    horizontal_movement = False
+                    direction = 4; horizontal_movement = False
 
-    if direction == 1:
-        x -= velocity
-    if direction == 2:
-        x += velocity
-    if direction == 3:
-        y -= velocity
-    if direction == 4:
-        y += velocity
-        
+    x, y = m.next_direction(x, y, direction, velocity)
+
     positions_x.append(x) # zapise do listu aktualni pozici
     positions_y.append(y)
     
@@ -67,27 +55,17 @@ while running:
         screen.blit(snake, (positions_x[i], positions_y[i]))
 
     if x == apple_x and y == apple_y:
-        apple_on_snake = True
-        while apple_on_snake:
-            apple_x = random.choice(apple_positons)
-            apple_y = random.choice(apple_positons)
-            apple_on_snake = False
-            for n in range(number_of_points):
-                if apple_x == positions_x[n] and apple_y == positions_y[n]:
-                    apple_on_snake = True
-
+        apple_x, apple_y = m.next_apple(apple_positons, number_of_points, positions_x, positions_y)
         number_of_points += 1
     else:
         positions_x.pop(0) # smaze z listu posledni pozici
         positions_y.pop(0)
 
-    for p in range (number_of_points - 1): # zde poznava zda nenarazil sam na sebe
-        if x == positions_x[p] and y == positions_y[p]:
-            print(p)
+    print(m.lose(number_of_points, x, y, positions_x, positions_y))
 
     screen.blit(apple, (apple_x, apple_y))
 
     pygame.display.update()
     pygame.time.wait(150)
-        
-    #ps: opravit to ze kdyz rychle zmenim smer had muze jit dozadu a kdyz rychle zmenim smer u jablka jablko se nevezme a preformatovat kod, protoze tohle je bordel
+
+#ps: opravit to ze kdyz rychle zmenim smer had muze jit dozadu a kdyz rychle zmenim smer u jablka jablko se nevezme
--- movement.py
+++ movement.py
@@ -1,9 +1,29 @@
-class move:
-    def __init__(self, direction = 0):
-        self._direction = direction
+import random
+def next_direction(x, y, direction, velocity):
+    if direction == 1:
+        x -= velocity
+    if direction == 2:
+        x += velocity
+    if direction == 3:
+        y -= velocity
+    if direction == 4:
+        y += velocity
+    return x, y
 
-    def get_direction(self):
-        return self._direction
+def next_apple(apple_positons, number_of_points, positions_x, positions_y):
+    apple_on_snake = True
+    while apple_on_snake:
+        apple_x = random.choice(apple_positons)
+        apple_y = random.choice(apple_positons)
+        apple_on_snake = False
+        for n in range(number_of_points):
+            if apple_x == positions_x[n] and apple_y == positions_y[n]:
+                apple_on_snake = True
 
-    def set_direction(self, x):
-        self._direction = x
+    return apple_x, apple_y
+
+def lose(number_of_points, x, y, positions_x, positions_y):
+    for p in range (number_of_points - 1): # zde poznava zda nenarazil sam na sebe
+        if x == positions_x[p] and y == positions_y[p]:
+            return 'lost'
+    return 'lol'
